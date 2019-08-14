3232d86bc2f688fdf744374da70cb6d20944b726
kylemonper <kyle.monper@gmail.com>
2019-08-14 13:08:09 -0700

    minor update

--- preproccess/drought_clim.R
+++ preproccess/drought_clim.R
@@ -1,3 +1,5 @@
+## this script is no longer reproducible because the original file was overwritten -- just a guide for how the data was spliced together
+
 library(tidyverse)
 library(RHESSysIOinR)
 
@@ -10,12 +12,9 @@ late <- read_rhessys_met('/Users/Kyle/Google Drive/JDSF/rhessys/clim/drought/had
 
 
 # change units from early and late century drought data from Kelvins and Meters to C and mm
-early <- mutate(early, rain = rain/1000, 
-                tmax = tmax-273.15, 
-                tmin = tmin-273.15)
-late <- mutate(late, rain = rain/1000,
-                tmax = tmax-273.15,
-                tmin = tmin-273.15)
+# early <- mutate(early, rain = rain/1000000)
+# late <- mutate(late, rain = rain/1000000)
+# had <- mutate(had, rain = rain/1000)
 
 ## alter the original 8.5 Had climate data to create two new datasets that have 'early' and 'late' droughts
 
@@ -34,12 +33,12 @@ late2 <- filter(had, had$date > max(late$date))
 late_drought <- bind_rows(list(late1, late, late2))
 
 
-write.table(late_drought$rain, '/Users/Kyle/Google Drive/JDSF/rhessys/clim/drought/had_late.rain')
-write.table(late_drought$tmin, '/Users/Kyle/Google Drive/JDSF/rhessys/clim/drought/had_late.tmin')
-write.table(late_drought$tmax, '/Users/Kyle/Google Drive/JDSF/rhessys/clim/drought/had_late.tmax')
+write.table(late_drought$rain, '/Users/Kyle/Google Drive/JDSF/rhessys/clim/drought/had_late.rain', row.names = F)
+write.table(late_drought$tmin, '/Users/Kyle/Google Drive/JDSF/rhessys/clim/drought/had_late.tmin', row.names = F)
+write.table(late_drought$tmax, '/Users/Kyle/Google Drive/JDSF/rhessys/clim/drought/had_late.tmax', row.names = F)
 
 
 
-write.table(early_drought$rain, '/Users/Kyle/Google Drive/JDSF/rhessys/clim/drought/had_early.rain')
-write.table(early_drought$tmin, '/Users/Kyle/Google Drive/JDSF/rhessys/clim/drought/had_early.tmin')
-write.table(early_drought$tmax, '/Users/Kyle/Google Drive/JDSF/rhessys/clim/drought/had_early.tmax')
+write.table(early_drought$rain, '/Users/Kyle/Google Drive/JDSF/rhessys/clim/drought/had_early.rain', row.names = F)
+write.table(early_drought$tmin, '/Users/Kyle/Google Drive/JDSF/rhessys/clim/drought/had_early.tmin', row.names = F)
+write.table(early_drought$tmax, '/Users/Kyle/Google Drive/JDSF/rhessys/clim/drought/had_early.tmax', row.names = F)
